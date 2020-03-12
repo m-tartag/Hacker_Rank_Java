9605ae139cf59159b93aaf08cfaab053469aa4d2
m-tartag <mattbrs51@gmail.com>
2020-03-12 10:55:41 -0400

    working on solution 22

---
+++ src/org/hacker/solutions/day_22/Solution.java
@@ -0,0 +1,30 @@
+package org.hacker.solutions.day_22;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+
+
+        // Shows Top - Most Recent Addition
+        System.out.println(stack.peek());
+
+
+
+    }
+
+
+
+
+
+
+
+}
